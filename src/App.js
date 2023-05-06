3644f8c16cd8c98188951c8778ccfb185cd4addb
Andrey Shamardak <andrey.shamardak@gmail.com>
2023-05-06 13:35:58 +0200

    moved fetch products to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
@@ -12,8 +12,70 @@ import Cart from './pages/Cart'
 import NotFound from './pages/NotFound'
 
 function App() {
+    const [products, setProducts] = useState([])
     const [wishCount, setWishCount] = useState(0)
     const [cartCount, setCartCount] = useState(0)
+
+    const getData = () => {
+        let countWishlist = 0
+        let countCart = 0
+        let wishlist = []
+        let cart = []
+        let productsUpdated
+
+        if (localStorage.getItem('wishlist')) {
+            wishlist = JSON.parse(localStorage.getItem('wishlist'))
+        }
+
+        if (localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart'))
+        }
+
+        fetch('data.json' // from /public folder
+            , {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => {
+
+                if (data.length > 0) {
+                    productsUpdated = data.map((product) => {
+                        if (wishlist.includes(product.id)) {
+                            product.wishlist = true
+                            countWishlist++
+
+                        } else {
+                            product.wishlist = false
+                        }
+
+                        if (cart.includes(product.id)) {
+                            product.cart = true
+                            countCart++
+
+                        } else {
+                            product.cart = false
+                        }
+
+                        return product
+                    })
+                    wishCountHandler(countWishlist)
+                    cartCountHandler(countCart)
+                    setProducts(productsUpdated)
+                }
+
+            });
+    }
+    useEffect(() => {
+        console.log("executed only once!");
+        getData()
+
+    }, [])
+
+
     const wishCountHandler = useCallback((amount = 1) => {
         if (amount === 1) {
             console.log('wishCountHandler +++')
@@ -40,7 +102,7 @@ function App() {
                     <Routes>
                         <Route
                             path="/"
-                            element={<Products wishCountHandler={wishCountHandler} cartCountHandler={cartCountHandler} />}
+                            element={<Products products={products} wishCountHandler={wishCountHandler} cartCountHandler={cartCountHandler} />}
                         />
                         <Route
                             path="/wishlist"
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,99 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import products from '../data.json';
+import React from 'react'
 import Product from './Product'
 
-const Products = ({ wishCountHandler, cartCountHandler }) => {
-    const [productsList, setProductsList] = useState([])
-
-    useEffect(() => {
-        let countWishlist = 0
-        let countCart = 0
-        let wishlist = []
-        let cart = []
-
-        if (localStorage.getItem('wishlist')) {
-            wishlist = JSON.parse(localStorage.getItem('wishlist'))
-        }
-
-        if (localStorage.getItem('cart')) {
-            cart = JSON.parse(localStorage.getItem('cart'))
-        }
-
-        let productsUpdated
-        productsUpdated = products.map((product) => {
-            if (wishlist.includes(product.id)) {
-                product.wishlist = true
-                countWishlist++
-
-            } else {
-                product.wishlist = false
-            }
-
-            if (cart.includes(product.id)) {
-                product.cart = true
-                countCart++
-
-            } else {
-                product.cart = false
-            }
-
-            return product
-        })
-        setProductsList(productsUpdated)
-        wishCountHandler(countWishlist)
-        cartCountHandler(countCart)
-    }, [wishCountHandler, cartCountHandler]);
-
-    // // 1: Fetch from local file
-    // const [products, setProducts] = useState([]);
-    // const getProducts = () => {
-    //     fetch('./data.json', {
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Accept': 'application/json'
-    //         }
-    //     }
-    //     )
-    //         .then(function (response) {
-    //             console.log(response)
-    //             return response.json();
-    //         })
-    //         .then(function (myJson) {
-    //             console.log(myJson);
-    //             setProducts(myJson)
-    //         });
-    // }
-    // useEffect(() => {
-    //     getProducts()
-    // }, [])
-
-
-    // // 2: Fetch from API
-    // const [products, setProducts] = useState([])
-    // const fetchProductData = () => {
-    //     fetch("https://fakestoreapi.com/products")
-    //         .then(response => {
-    //             return response.json()
-    //         })
-    //         .then(data => {
-    //             setProducts(data)
-    //         })
-    // }
-
-    // useEffect(() => {
-    //     fetchProductData()
-    // }, [])
-
+const Products = ({ products, wishCountHandler, cartCountHandler }) => {
     return (
         <main>
-            {productsList.length > 0 && (
-                <div className='row'>
-                    {productsList.map(product => (
-                        <div className='col-md-6 col-lg-4 col-xl-3 mb-4' key={product.id}>
-                            <Product product={product} wishCountHandler={wishCountHandler} cartCountHandler={cartCountHandler} />
-                        </div>
-                    ))}
-                </div>
+            {products.length > 0 && (
+                <>
+                    <h1>Products</h1>
+                    <div className='row'>
+                        {products.map(product => (
+                            <div className='col-md-6 col-lg-4 col-xl-3 mb-4' key={product.id}>
+                                <Product product={product} wishCountHandler={wishCountHandler} cartCountHandler={cartCountHandler} />
+                            </div>
+                        ))}
+                    </div>
+                </>
             )}
         </main>
     )
